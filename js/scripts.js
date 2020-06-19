9c63a8b8804dba0bc4550fce2b0917510b1175d3
Rick <raisoncompany@gmail.com>
2020-06-19 20:43:57 +0500

    Revert "tak-blet"
    This reverts commit 5c0352d51446896c5c0eb776688d8467f1cb1083.

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-city_images = document.querySelector(".main-reasons-list__item_city-img")
-
-if (window.matchMedia("(min-width: 770px)").matches) {
-	city_images.src="images/photo-city.jpg"
-	console.log("Hyi")
-} else {
-	city_images.src="images/photo-city-mobile.jpg"
-}
-
