754db40cfb0ad12f4d8328fb5d89b0e0f9d6a5a5
Rick96b <olegkorobeinikov21@gmail.com>
2023-09-23 19:18:51 +0500

    last version

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 city_images = document.querySelector(".main-reasons-list__item_city-img")
 
-if (window.matchMedia("(min-width: 770px)").matches) {
-	city_images.src="images/photo-city.jpg"
-	console.log("Hyi")
-} else {
-	city_images.src="images/photo-city-mobile.jpg"
-}
 
+  if (window.matchMedia("(min-width: 940px)").matches) {
+    city_images.src="images/photo-city.jpg"
+    console.log("Hyi")
+  } else if (window.matchMedia("(min-width: 770px)").matches) {
+    city_images.src="images/photo-city-tablet.jpg"
+  } else {
+    city_images.src="images/photo-city-mobile.jpg"
+  }
